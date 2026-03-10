c568913deb1ea9b9ae32537e61aec866c1bb7b7e
rnemzek <rnemzek@gmail.com>
2026-03-10 18:24:38 -0400

    Removed unnecessary files

--- src/tests/test_github_get_actions.py
+++
@@ -1,9 +0,0 @@
-# THIS IS BUSTICATED
-from composio import ComposioToolSet, App
-
-toolset = ComposioToolSet()
-actions = toolset.get_actions(handle=App.GITHUB)
-
-for action in actions:
-    print(f"Name: {action.name}, Slug: {action.slug}")
-
